c2d11464db87981baa886ffa2b989337af0b2c43
fafirooz <fa.firooz@gmail.com>
2023-06-23 16:01:59 -0700

    edit

--- src/Array.java
+++ src/Array.java
@@ -1,9 +1,9 @@
 public class Array {
     public static void main(String[] args) {
-        int[] arrayList = {0, 1, 2, 3};
+        int[] arrayList = new int[4];
+        /* arrayList = new int[4]; */
+        /*int[] arrayList = {0, 1, 2, 3};*/
 
-
-        arrayList = new int[4];
 
         arrayList[0] = 1;
         arrayList[1] = 2;

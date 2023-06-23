a5675ea33187d3862bc1c9beb37c8903268d7866
fafirooz <fa.firooz@gmail.com>
2023-06-23 15:52:25 -0700

    First commit

---
+++ src/Array.java
@@ -0,0 +1,22 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] arrayList = {0, 1, 2, 3};
+
+
+        arrayList = new int[4];
+
+        arrayList[0] = 1;
+        arrayList[1] = 2;
+        arrayList[2] = 3;
+        arrayList[3] = 4;
+
+        System.out.println(arrayList[1]);
+        System.out.println(arrayList[0]);
+        System.out.println(arrayList[3]);
+
+
+    }
+
+
+}
+

344e8b43d711fd76b3149a38519aaef352c48667
fafirooz <fa.firooz@gmail.com>
2023-06-23 13:39:31 -0700

    First commit

---
+++ src/Human.java
@@ -0,0 +1,27 @@
+public class Human {
+    int weight;
+    int height;
+
+
+    public Human(int weight, int height) {
+        this.weight = weight;
+        this.height= height;
+    }
+
+    public void print(){
+        System.out.println("wheight is " + weight);
+        System.out.println("hieght is " + height);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+       Human fariba = new Human( 65,100 );
+       Human saeed = new Human(65 , 90 );
+       fariba.print();
+       saeed.print();
+    }
+}

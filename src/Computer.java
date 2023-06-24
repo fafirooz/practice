ef570f8432b7c0a387bf13e5bd5c8eb0b3744bc5
fafirooz <fa.firooz@gmail.com>
2023-06-24 15:58:20 -0700

    add commit

--- src/Computer.java
+++ src/Computer.java
@@ -3,4 +3,29 @@ public class Computer {
     String motherboard;
     String cpu;
     String gpu;
+
+
+    public Computer(){
+        System.out.println("This computer is:");
+    }
+
+    public Computer(String cpu01){
+        this.cpu= cpu01;
+        System.out.println("The type of cpu is:" + cpu01);
+    }
+
+    void info() {
+        System.out.println("the ram is:" + cpu + " the motherboard is:" + motherboard);
+
+    }
+
+    String show() {
+        return "the ram is:" + ram + " the motherboard is:" + motherboard;
+
+    }
+
+    int addition(int firstNumber, int secoundNumber) {
+        return firstNumber + secoundNumber;
+
+    }
 }

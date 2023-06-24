793d0a8f6f749df800a25bd2877b0bdcde125f38
fafirooz <fa.firooz@gmail.com>
2023-06-23 20:25:44 -0700

    first commit

---
+++ src/While.java
@@ -0,0 +1,15 @@
+public class While {
+    public static void main(String[] args) {
+        int value01 = 10;
+        do {
+            System.out.println("value is:" + value01);
+            value01++;
+        }
+        while (value01 < 12);
+
+    }
+
+
+}
+
+

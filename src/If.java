ffb9793d9ad4d8adf2fe6e9a0c0ae3d0a40d1552
fafirooz <fa.firooz@gmail.com>
2023-06-23 19:34:39 -0700

    first commit

---
+++ src/If.java
@@ -0,0 +1,16 @@
+public class If {
+    public static void main(String[] args) {
+        int variable= 10;
+
+        if(variable < 3){
+            System.out.println("variable is less than 3");
+        }
+        else if (variable == 10) {
+            System.out.println("10");
+        }
+        else {
+            System.out.println("variable is bigger than 3");
+        }
+
+    }
+}

c785ce4209fa57960ebec5da6be1a35e444db15a
fafirooz <fa.firooz@gmail.com>
2023-06-23 20:01:27 -0700

    first commit

---
+++ src/Loop.java
@@ -0,0 +1,14 @@
+public class Loop {
+    public static void main(String[] args) {
+        int [] arrayList = {1,2,3,4,5,6};
+        for (int i =1; i < arrayList.length; i+=1){
+            System.out.println(arrayList[i]);
+            for (int c= 0 ; c< arrayList.length ; c++){
+                System.out.println("inner loop");
+            }
+
+        }
+
+    }
+
+}

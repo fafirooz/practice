1d1697337ef957b0873303983f5a39aefb133fc9
fafirooz <fa.firooz@gmail.com>
2023-06-23 21:27:51 -0700

    first commit

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+    public static void main(Application[] args) {
+        String text = new String("hello");
+        text= "hello";
+
+        String text02 = text + "baby";
+
+
+
+    }
+}

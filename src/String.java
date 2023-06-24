9958517614935891671bbf4a8bbe504fd8d88f95
fafirooz <fa.firooz@gmail.com>
2023-06-23 21:21:09 -0700

    first commit

---
+++ src/String.java
@@ -0,0 +1,10 @@
+public class String {
+    public static void main(String[] args) {
+        String text = new String();
+        text= "text";
+        Integer num = 12;
+        text= num.toString();
+        System.out.println(text);
+
+    }
+}

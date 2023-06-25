929dd09c57970a80a56cbe17bb3d7a768f6ad351
fafirooz <fa.firooz@gmail.com>
2023-06-24 23:15:38 -0700

    first commit

---
+++ src/NewApp.java
@@ -0,0 +1,6 @@
+public class NewApp {
+    public static void main(String[] args) {
+        Computer computer01= new Computer();
+        System.out.println(NewComputer.type);
+    }
+}
---
+++ src/NewComputer.java
@@ -0,0 +1,8 @@
+public class NewComputer {
+    int ram;
+    String motherboard;
+    String cpu;
+    String gpu;
+
+    public static String type = "Fariba Firooz";
+}

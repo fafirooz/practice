a223690abb38f1d340c2b1c9e5472f6287218408
fafirooz <fa.firooz@gmail.com>
2023-07-01 10:28:03 -0700

    commit

--- src/AppEqual.java
+++ src/AppEqual.java
@@ -2,8 +2,6 @@ public class AppEqual {
     public static void main(String[] args) {
         LaptopPoly laptop01 = new LaptopPoly();
         LaptopPoly laptop02 = new LaptopPoly();
-        laptop01.setManufacture("HP");
-        laptop02.setManufacture("HP");
         System.out.println(laptop02.equals(laptop01));
 
     }
